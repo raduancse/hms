f81273174d35106f6fe6aad33b108604c5e5b2c9
Md. Raduan Islam <raduan.cse@gmail.com>
2022-07-21 13:19:04 +0600

    admin sidebar active done

--- hms/admin/dashboard.php
+++ hms/admin/dashboard.php
@@ -145,6 +145,7 @@ check_login();
             <!-- end: SETTINGS -->
         </div>
         <!-- start: MAIN JAVASCRIPTS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="vendor/jquery/jquery.min.js"></script>
         <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
         <script src="vendor/modernizr/modernizr.js"></script>
